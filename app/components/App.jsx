c14994c728da0409dbaba92a28fcc74b6f2df29a
Robbie Netzorg <robbizorg@gmail.com>
2016-05-17 22:16:04 -0400

    Now can Edit Notes

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,8 +1,63 @@
+import uuid from 'node-uuid';
 import React from 'react';
-import Note from './Note.jsx';
+import Notes from './Notes.jsx';
 
 export default class App extends React.Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			notes: [
+				{
+					id: uuid.v4(),
+					task: 'Learn Webpack'
+				},
+				{
+					id: uuid.v4(),
+					task: 'Learn React'
+				},
+				{
+					id: uuid.v4(),
+					task: 'Do Laundry'
+				}
+			]
+		};
+	}
 	render() {
-		return <Note />;
+		const notes = this.state.notes;
+
+		return (
+			<div>
+				<button onClick={this.addNote}>+</button>
+				<Notes notes={notes} onEdit={this.editNote}/>
+			</div>
+		)
+		
+	}
+
+	addNote = () => {
+		this.setState({
+			notes: this.state.notes.concat([{
+				id: uuid.v4(),
+				task: 'New Task'
+			}])
+		}, console.log("State Changed"))
+	};
+	editNote = (id, task) => {
+		// Don't modify if empty value
+		console.log(task);
+		console.log(id);
+		if(!task.trim()) {
+			return;
+		}
+
+		const notes = this.state.notes.map(note => {
+			if(note.id === id && task) {
+				note.task = task;
+			}
+			return note;
+		});
+
+		this.setState({notes});
 	}
 }
--- app/components/Note.jsx
+++ app/components/Note.jsx
@@ -1,3 +1,59 @@
 import React from 'react';
 
-export default () => <div>Did it work?</div>;
+export default class Note extends React.Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			editing: false
+		};
+	}
+	render() {
+		if(this.state.editing) {
+			return this.renderEdit();
+		}
+
+		return this.renderNote();
+	}
+	renderEdit = () => {
+		// Look into Refs a bit more
+		return <input type="text"
+			ref={
+				element => element ?
+				element.selectionStart = this.props.task.length : null
+			}
+			autoFocus={true}
+			defaultValue={this.props.task}
+			onBlur={this.finishEdit}
+			onKeyPress={this.checkEnter} />;
+	};
+	renderNote = () => {
+		// If the user clicks a normal note, trigger edit
+		return <div onClick={this.edit}>{this.props.task}</div>;
+	};
+	edit = () => {
+		// enter edit mode
+		this.setState({
+			editing: true
+		});
+	};
+	checkEnter = (e) => {
+		if (e.key === 'Enter') {
+			this.finishEdit(e);
+		}
+	};
+	finishEdit = (e) => {
+		// Note will trigger optional onEdit callback
+		// to communicate change to app
+
+		const value = e.target.value;
+
+		if(this.props.onEdit) {
+			this.props.onEdit(value);
+
+			this.setState({
+				editing: false
+			});
+		}
+	};
+}
