758af8bfc242051e50d6cfbced246095d4333915
Robbie Netzorg <robbizorg@gmail.com>
2016-05-19 16:07:11 -0400

    Added localStorage

--- app/index.jsx
+++ app/index.jsx
@@ -3,6 +3,11 @@ import './main.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App.jsx';
+import alt from './libs/alt';
+import storage from './libs/storage';
+import persist from './libs/persist';
+
+persist(alt, storage, 'app');
 
 // Avoid rendering directly to document.body
 // Can cause strange problems when using react
