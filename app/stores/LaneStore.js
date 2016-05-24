5c04dd5789ee26ffe61215980e061bb994af4223
Robbie Netzorg <robbizorg@gmail.com>
2016-05-23 22:26:50 -0400

    Added Lanes and New Notes

---
+++ app/stores/LaneStore.js
@@ -0,0 +1,57 @@
+import uuid from 'node-uuid';
+import alt from '../libs/alt';
+import LaneActions from '../actions/LaneActions';
+
+// If working with Backend, must implement waitFor method
+// in order to allow rendering after data has loaded
+class LaneStore {
+	constructor() {
+		this.bindActions(LaneActions);
+
+		this.lanes = [];
+	}
+	create(lane) {
+		const lanes = this.lanes;
+
+		lane.id = uuid.v4();
+
+		// If notes aren't provided, 
+		// default to empty array
+		lane.notes = lane.notes || [];
+
+		this.setState({
+			lanes: lanes.concat(lane)
+		});
+	}
+	attachToLane({noteId, laneId}) {
+		console.log("Im Being Called");
+		const lanes = this.lanes.map(lane => {
+			console.log("Iterating through lanes");
+			if (lane.id === laneId) {
+				console.log("Its a match!");
+				if(lane.notes.includes(noteId)) {
+					console.warn('Already attached note to lane', lanes);
+				} else {
+					console.log("Pushing Notes");
+					lane.notes.push(noteId);
+				}
+			}
+			return lane;
+		});
+
+		this.setState({lanes});
+	}
+	detachFromLane({laneId, noteId}) {
+		const lanes = this.lanes.map(lane => {
+			if (lane.id === laneId) {
+				lane.notes = lane.notes.filter(note => note !== noteId);
+			}
+
+			return lane;
+		});
+
+		this.setState({lanes});
+	}
+}
+
+export default alt.createStore(LaneStore, 'LaneStore');
--- app/stores/NoteStore.js
+++ app/stores/NoteStore.js
@@ -7,6 +7,10 @@ class NoteStore {
 		this.bindActions(NoteActions);
 
 		this.notes = [];
+
+		this.exportPublicMethods({
+			getNotesByIds: this.getNotesByIds.bind(this)
+		});
 	}
 	create(note) {
 		const notes = this.notes;
@@ -16,6 +20,8 @@ class NoteStore {
 		this.setState({
 			notes: notes.concat(note)
 		});
+
+		return note;
 	}
 	update(updatedNote) {
 		const notes = this.notes.map(note => {
@@ -29,12 +35,20 @@ class NoteStore {
 
 		// Same as setState({notes: notes})
 		// It's an ES6 feature known as property shorthand
-		this.setState({notes})
+		this.setState({notes});
 	}
 	delete(id) {
 		this.setState({
 			notes: this.notes.filter(note => note.id !== id)
 		});
+	}
+	getNotesByIds(ids) {
+		// Make sure that we're operating on an array and map over the ids
+		return (ids || []).map(
+			// Extract matching notes
+			id => this.notes.filter(note => note.id === id)
+			// Filter out empty arrays and get notes
+		).filter(a => a.length).map(a => a[0]);
 	}
 }
 
